a7cbb9aec8467c5e93ed06de588d7bbef36fc8e7
pandacsenke <podacsenge@gmail.com>
2023-04-13 21:05:59 +0200

    test_11 - 10.assert nincs

--- Test/test_conduit.py
+++ Test/test_conduit.py
@@ -37,10 +37,10 @@ class TestConduit(object):
         options = Options()
         options.add_experimental_option("detach", True)
 
-        # # For GitHub Actions
-        # options.add_argument('--headless')
-        # options.add_argument('--no-sandbox')
-        # options.add_argument('--disable-dev-shm-usage')
+        # For GitHub Actions
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
 
         self.browser = webdriver.Chrome(service=service, options=options)
         URL = "http://localhost:1667"
